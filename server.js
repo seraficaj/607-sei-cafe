3f71f84ffb1ad12474fd9fbe0044ca1a0f87df06
Daniel Beck <dlc.beck@gmail.com>
2021-07-21 11:32:50 -0700

    adds crud-helper

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const logger = require('morgan');
 // Always require and configure near the top
 require('dotenv').config();
 
+// Connect to the database
+require('./config/database');
+
 const app = express();
 
 app.use(logger('dev'));
