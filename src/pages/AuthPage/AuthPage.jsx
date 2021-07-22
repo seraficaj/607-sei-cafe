85369f0c10adb7eba79292057c3ff07bec725b74
Daniel Beck <dlc.beck@gmail.com>
2021-07-22 14:26:37 -0700

    adds signupform and general app css

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,5 +1,12 @@
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+
 function AuthPage() {
-	return <h1>AuthPage</h1>;
+	return (
+		<main>
+			<h1>AuthPage</h1>
+			<SignUpForm />
+		</main>
+	);
 }
 
 export default AuthPage;
