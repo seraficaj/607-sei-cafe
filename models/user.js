4c008689fc421a1cdd17fdf50cf8ae907246ca72
Daniel Beck <dlc.beck@gmail.com>
2021-07-23 14:04:33 -0700

    adds user model

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+			lowercase: true,
+		},
+		password: {
+			type: String,
+			trim: true,
+			minLength: 3,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+		// Even though it's hashed - don't serialize the password
+		toJSON: {
+			transform: function (doc, ret) {
+				delete ret.password;
+				return ret;
+			},
+		},
+	}
+);
+
+module.exports = mongoose.model('User', userSchema);
