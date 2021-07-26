7e6c4b9ea5fa32f719ed4b6bba76416c36dc1922
Daniel Beck <dlc.beck@gmail.com>
2021-07-26 13:48:19 -0700

    stubs up checktoken handler

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,5 +1,16 @@
 function OrderHistoryPage() {
-	return <h1>OrderHistoryPage</h1>;
+	async function handleCheckToken() {
+		alert('clicked');
+	}
+
+	return (
+		<>
+			<h1>OrderHistoryPage</h1>
+			<button onClick={handleCheckToken}>
+				Check When My Login Expires
+			</button>
+		</>
+	);
 }
 
 export default OrderHistoryPage;
