b7a0c396f222e7e376f011379145fe0c3f24a350
Daniel Beck <dlc.beck@gmail.com>
2021-07-26 09:25:02 -0700

    adds getToken and getUser functions to persist token in local storage and retrieve user data

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -7,9 +7,31 @@ export async function signUp(userData) {
 		// Delegate the network request code to the users-api.js API module
 		// which will ultimately return a JSON Web Token (JWT)
 		const token = await usersAPI.signUp(userData);
+		// Persist the "token"
+		localStorage.setItem('token', token);
 		// Baby step by returning whaterver is sent back to the server
 		return token;
 	} catch {
 		throw new Error('Invalid Sign Up');
 	}
+}
+
+export function getToken() {
+	// getItem returns null if there's no string
+	const token = localStorage.getItem('token');
+	if (!token) return null;
+	// Check if expired, remove if it is
+	const payload = JSON.parse(atob(token.split('.')[1]));
+	// A JWT's exp is expressed in seconds, not milliseconds, so convert it
+	if (payload.exp < Date.now() / 1000) {
+		localStorage.removeItem('token');
+		return null;
+	}
+	return token;
+}
+
+export function getUser() {
+	const token = getToken();
+	// If there's a token, return the user in the payload, otherwise return null
+	return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
