316da06db16768fcb2d2baf251edba5db8c79c7d
Jason Serafica <seraficaj@gmail.com>
2021-07-26 09:50:44 -0700

    set user

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
@@ -30,39 +29,6 @@ function App() {
 			)}
 		</main>
 	);
-=======
-import { useState } from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
-import AuthPage from "../AuthPage/AuthPage";
-import NewOrderPage from "../NewOrderPage/NewOrderPage";
-import NavBar from "../../components/NavBar/NavBar";
-import OrderHistoryPage from "../OrderHistoryPage.jsx/OrderHistoryPage";
-import "./App.css";
-
-function App() {
-  const [user, setUser] = useState(null);
-
-  return (
-    <main className="App">
-      {user ? (
-        <>
-          <NavBar />
-          <Switch>
-            <Route path="/orders/new">
-              <NewOrderPage />
-            </Route>
-            <Route path="/orders">
-              <OrderHistoryPage />
-            </Route>
-            <Redirect to="/orders" />
-          </Switch>
-        </>
-      ) : (
-        <AuthPage />
-      )}
-    </main>
-  );
->>>>>>> cc9a93c4b76c4fcce5e6c92294d242d15637178f
 }
 
 export default App;
