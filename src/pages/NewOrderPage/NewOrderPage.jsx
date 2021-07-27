8834f6c6480cb509d0732eb90ffcbe24e2064879
Daniel Beck <dlc.beck@gmail.com>
2021-07-27 14:23:34 -0700

    fixes errors by passing props to NewOrderPage

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,23 +1,56 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
+// Add the component imports
+import './NewOrderPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
-export default function NewOrderPage() {
+export default function NewOrderPage({ user, setUser }) {
 	const [menuItems, setMenuItems] = useState([]);
+	// Add state to track the "active" category
+	const [activeCat, setActiveCat] = useState('');
+
+	// Create and initialize the ref to an empty array
+	const categoriesRef = useRef([]);
 
 	useEffect(() => {
 		async function getItems() {
 			const items = await itemsAPI.getAll();
+			categoriesRef.current = items.reduce((cats, item) => {
+				const cat = item.category.name;
+				return cats.includes(cat) ? cats : [...cats, cat];
+			}, []);
 			setMenuItems(items);
+			// Add this line to initialize the active category
+			setActiveCat(items[0].category.name);
 		}
 		getItems();
 	}, []);
 
 	return (
-		<>
-			<h1>NewOrderPage</h1>
-			<button onClick={() => setMenuItems(Date.now())}>
-				Trigger re-render
-			</button>
-		</>
+		<main className='NewOrderPage'>
+			<aside>
+				<Logo />
+				<CategoryList
+					categories={categoriesRef.current}
+					activeCat={activeCat}
+					setActiveCat={setActiveCat}
+				/>
+				<Link to='/orders' className='button btn-sm'>
+					PREVIOUS ORDERS
+				</Link>
+				<UserLogOut user={user} setUser={setUser} />
+			</aside>
+			<MenuList
+				menuItems={menuItems.filter(
+					item => item.category.name === activeCat
+				)}
+			/>
+			<OrderDetail />
+		</main>
 	);
 }
