e116bd05497fdbcf138235656a5e49b532686623
Jason Serafica <seraficaj@gmail.com>
2021-07-27 15:21:58 -0700

    w08d2

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,7 +5,6 @@ import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
 	const [user, setUser] = useState(getUser());
@@ -14,10 +13,9 @@ export default function App() {
 		<main className='App'>
 			{user ? (
 				<>
-					<NavBar user={user} setUser={setUser} />
 					<Switch>
 						<Route path='/orders/new'>
-							<NewOrderPage />
+							<NewOrderPage user={user} setUser={setUser}/>
 						</Route>
 						<Route path='/orders'>
 							<OrderHistoryPage />
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,23 +1,55 @@
-import { useState, useEffect } from "react";
-import * as itemsApi from '../../utilities/items-api';
+import { useState, useEffect, useRef } from "react";
+import * as itemsApi from "../../utilities/items-api";
+// Add the component imports
+import "./NewOrderPage.css";
+import { Link } from "react-router-dom";
+import Logo from "../../components/Logo/Logo";
+import MenuList from "../../components/MenuList/MenuList";
+import CategoryList from "../../components/CategoryList/CategoryList";
+import OrderDetail from "../../components/OrderDetail/OrderDetail";
+import UserLogOut from "../../components/UserLogOut/UserLogOut";
 
-export default function NewOrderPage() {
+export default function NewOrderPage({user, setUser}) {
 	const [menuItems, setMenuItems] = useState([]);
+  // Add state to track active Category
+  const [activeCat, setActiveCat] = useState('');
+	// Create and initialize the ref to an empty array
+	const categoriesRef = useRef([]);
 
 	useEffect(() => {
-    async function getItems() {
-      const items = await itemsApi.getAll();
-      setMenuItems(items);
-    }
-    getItems();
+		async function getItems() {
+			const items = await itemsApi.getAll();
+			categoriesRef.current = items.reduce((cats, item) => {
+				const cat = item.category.name;
+				return cats.includes(cat) ? cats : [...cats, cat];
+			}, []);
+			setMenuItems(items);
+      // Add this line to initialize the active category
+      setActiveCat(items[0].category.name);
+		}
+		getItems();
 	}, []);
 
 	return (
-		<>
-			<h1>NewOrderPage</h1>
-			<button onClick={() => setMenuItems(Date.now())}>
-				Trigger Re-render
-			</button>
-		</>
+		<main className="NewOrderPage">
+			<aside>
+				<Logo />
+				<CategoryList
+					categories={categoriesRef.current}
+					activeCat={activeCat}
+					setActiveCat={setActiveCat}
+				/>
+				<Link to="/orders" className="button btn-sm">
+					PREVIOUS ORDERS
+				</Link>
+				<UserLogOut user={user} setUser={setUser} />
+			</aside>
+			<MenuList
+				menuItems={menuItems.filter(
+					(item) => item.category.name === activeCat
+				)}
+			/>
+			<OrderDetail />
+		</main>
 	);
 }
