91484fa623e25b97daf2bb67885633ecc2942fc4
Daniel Beck <dlc.beck@gmail.com>
2021-07-27 08:46:03 -0700

    updates page level component for lesson

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+
 export default function NewOrderPage() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  // - Fetch the menuItems from the server via AJAX
+  // - When the data comes back, call setMenuItems to save in state
+
   return (
     <h1>NewOrderPage</h1>
   );
