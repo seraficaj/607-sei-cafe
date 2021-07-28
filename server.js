9c40cc52ab1125c7bd4b920903e213ae34f8ed00
Jason Serafica <seraficaj@gmail.com>
2021-07-28 09:13:35 -0700

    merge w08d3 starter code

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
 
 // Protect the api routes below from anonymous users
-const ensureLoggedIn = require('./config/ensureLoggedIn');
 app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
 app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
 
